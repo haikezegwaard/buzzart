e7f8d9ec72ef0d6e89790528cda495879626e0a9
haike <haike@16672f36-3850-4276-9a78-bc8ed93f5030>
2014-09-15 11:38:50 +0000

    'fix'
    git-svn-id: svn://svn.fam/data/svn/buzzart@11 16672f36-3850-4276-9a78-bc8ed93f5030

--- trunk/monitor/cron.py
+++ trunk/monitor/cron.py
@@ -1,3 +1,7 @@
-
+from monitor.models import Summary
 
 class Cron:
+
+    def create_summary(self, project):
+        """Create a summary object for given project
+        Summary is a collection of statistics """
