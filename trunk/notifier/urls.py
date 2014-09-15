19aa5d4daf7f5eb5184bdef90a44b15109ce3ac2
haike <haike@16672f36-3850-4276-9a78-bc8ed93f5030>
2014-09-15 14:40:14 +0000

    canvas
    git-svn-id: svn://svn.fam/data/svn/buzzart@24 16672f36-3850-4276-9a78-bc8ed93f5030

--- trunk/notifier/urls.py
+++ trunk/notifier/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
-from views import DigestView
+from views import DigestView, DirectTemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'canvas$', DirectTemplateView.as_view(template_name='canvas.html', extra_context={ 'projectname': 'testproject'}),  name='canvas'),
     url(r'^$', DigestView.as_view(template_name='digest.html', extra_context={ 'projectname': 'testproject'}),  name='digest')
 )
