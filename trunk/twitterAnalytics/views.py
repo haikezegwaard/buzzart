6a5c4c8ff4296184793c3ae5d61615eba62d048d
haike <haike@ubuntu.ubuntu-domain>
2014-10-12 14:39:44 +0200

    twitter key fix

--- trunk/twitterAnalytics/views.py
+++ trunk/twitterAnalytics/views.py
@@ -5,12 +5,13 @@ from twitter import *
 # Create your views here.
 def index(request):
 
-    token = '15916075-vCZIeZn3f4ksDz3Qg8RRgk4opE0rqhkOHkSR2GE4x'
-    token_key = 'Emoq1PJAn98K2mbjBT6hXkfxpmllMLuxwWFYZpBYOr4eD'
-    con_secret = 'nyylZGieWcZYpylxocCGySDv5rcHwgTMXK9MOmeIbjqBbBKigW'
-    con_secret_key = 'pfhysTbUY4iTCVpdS3MwXHWPc'
+    oauth_token = '15916075-vCZIeZn3f4ksDz3Qg8RRgk4opE0rqhkOHkSR2GE4x'
+    oauth_secret = 'Emoq1PJAn98K2mbjBT6hXkfxpmllMLuxwWFYZpBYOr4eD'
+    consumer_secret = 'nyylZGieWcZYpylxocCGySDv5rcHwgTMXK9MOmeIbjqBbBKigW'
+    consumer_key = 'pfhysTbUY4iTCVpdS3MwXHWPc'
 
 
 
-    t = Twitter(auth=OAuth(token, token_key, con_secret, con_secret_key)))
-    return HttpResponse('test', mimetype='text/html')
+    t = Twitter(auth=OAuth(oauth_token, oauth_secret,
+                           consumer_secret, consumer_key))
+    return HttpResponse(t.statuses.home_timeline(), mimetype='text/html')
