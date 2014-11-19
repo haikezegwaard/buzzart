8b14c75b073c76ae1034c62bdd5ca0374855fb88
haikezegwaard <h.zegwaard@gmail.com>
2014-11-19 12:06:53 +0100

    views

--- trunk/facebookAds/views.py
+++ trunk/facebookAds/views.py
@@ -3,22 +3,10 @@ from django.conf import settings
 from facebookads import objects
 from facebookads.api import FacebookAdsApi
 from fbadsmanager import FacebookAdsManager
+from facebookads.objects import AdCampaign
 import logging
 # Create your views here.
 
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    FacebookAdsApi.init(settings.FACEBOOK_ADS_APP_ID,
-                        settings.FACEBOOK_ADS_APP_SECRET,
-                        settings.FACEBOOK_ADS_ACCESS_TOKEN)
-    me = objects.AdUser(fbid='me')
-    my_accounts = list(me.get_ad_accounts())
-    logger.debug(my_accounts)
-    my_account = objects.AdAccount('act_52022373')
-    fbman = FacebookAdsManager()
-    stats = fbman.campaign_stats(my_account)
-    return render_to_response('facebookAds/index.html',
-                              {"stats": stats},
-                              content_type="text/html")
