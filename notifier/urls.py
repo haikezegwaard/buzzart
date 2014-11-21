a20f36e45edcb4ba0844c6913fc4995e9c544cfc
haikezegwaard <h.zegwaard@gmail.com>
2014-11-21 17:01:22 +0100

    added project test method

--- notifier/urls.py
+++ notifier/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
-from views import DigestView, DirectTemplateView, MailView
+from views import DigestView, DigestTestView, MailView
 import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'(?P<pk>\d+)/mailing', MailView.as_view(template_name='mailing.html', extra_context={ 'fallback': 1}),  name='mailing'),
+    url(r'(?P<pk>\d+)/test', DigestTestView.as_view(template_name='digest.html', extra_context={ 'fallback': 1}),  name='testdigest'),
     url(r'^(?P<pk>\d+)$', DigestView.as_view(template_name='digest.html', extra_context={ 'fallback': 1}),  name='digest'),
 )
