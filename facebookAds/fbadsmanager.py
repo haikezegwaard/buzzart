4d1b83b5475a52403b023d8f2059d2073c6b60eb
haikezegwaard <hz@fundament.nl>
2014-11-23 15:37:06 +0100

    code cleaning

--- facebookAds/fbadsmanager.py
+++ facebookAds/fbadsmanager.py
@@ -3,9 +3,11 @@ from facebookads import objects
 from django.conf import settings
 from facebookads.objects import AdCampaign, ReportStats
 from models import FacebookAdsSettings
+from urlparse import parse_qs, urlparse
 import requests
 import logging
 import json
+from datetime import datetime, timedelta
 import hmac
 import hashlib
 
@@ -21,18 +23,18 @@ class FacebookAdsManager:
         self.logger = logging.getLogger(__name__)
         if FacebookAdsSettings.objects.all().count() == 0:
             self.generate_long_lived_token(token)
+        if FacebookAdsSettings.objects.all().count() > 1:
+            raise Exception('''Found more than one Facebook Ad access token
+                               in the database, this should not happen!''')
 
 
     def get_async_report(self, job):
         params={'report_run_id': job}
-        graph_object = 'act_52022373/reportstats'
-        response = self.API_call(graph_object, params)
-        return response
+        graph_object='act_52022373/reportstats'
+        return self.API_call(graph_object, params)
 
     def get_campaign_stats(self, async='false'):
-
         graph_object = 'act_52022373/reportstats'
-
         pars={'data_columns': "['account_id','spend','action_values']",
               'time_ranges': "[{'day_start':{'day':1,'month':3,'year':2010},'day_stop':{'day':27,'month':3,'year':2012}}]",
               'actions_group_by': "['action_type']"}
@@ -56,7 +58,6 @@ class FacebookAdsManager:
         fbsettings = FacebookAdsSettings.objects.first()
         return fbsettings.access_token
 
-
     def get_app_proof(self):
         """
         Generate the App secret proof, this is sent with every request
@@ -66,7 +67,6 @@ class FacebookAdsManager:
         key = settings.FACEBOOK_ADS_APP_SECRET
         data = self.get_stored_token()
         return hmac.new(key, data, hashlib.sha256).hexdigest()
-
 
     def generate_long_lived_token(self, short_token):
         """
@@ -79,14 +79,11 @@ class FacebookAdsManager:
                                              'grant_type': 'fb_exchange_token',
                                              'fb_exchange_token': short_token})
         self.logger.debug(response.content)
-        from urlparse import parse_qs, urlparse
         dictresponse = parse_qs(urlparse('?{}'.format(response.content)).query, keep_blank_values=True)
         self.logger.debug(dictresponse)
         if response.status_code == 200:
             token = dictresponse.get('access_token')[0]
-            from datetime import datetime, timedelta
             expires = datetime.now() + timedelta(seconds=int(dictresponse.get('expires')[0]))
-
             FacebookAdsSettings.objects.create(access_token=token, expires=expires)
             return token
         else:
