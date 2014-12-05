c3ed358d8994fb6aea28b1a227eed15d0fd3c6e3
haikezegwaard <h.zegwaard@gmail.com>
2014-12-05 16:44:19 +0100

    added extra templates

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 import views
 admin.autodiscover()
 
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^(?P<project_id>[0-9]*)$', views.index, name='home'),
     url(r'^(?P<project_id>[0-9]*)/origin$', views.origin, name='origin'),
     url(r'^sitelist$', views.site_list, name='site_list'),
-    url(r'^updates', views.updates, name='updates')
+    url(r'^updates', views.updates, name='updates'),
+    url(r'^FRX/radyus$', TemplateView.as_view(template_name='FRX/radyus/index.html')),
 )
