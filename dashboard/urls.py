50e5e26897b91f18d6f84c31d61ea7a83ef6af1a
haikezegwaard <hz@fundament.nl>
2014-12-16 14:41:49 +0100

    cleanup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,9 +6,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^plotdata/(?P<project_id>[0-9]*)', views.plottingDataSeries, name='plottingData'),
-    url(r'^(?P<project_id>[0-9]*)$', views.index, name='home'),
+    url(r'^(?P<project_id>[0-9]*)$', views.index, name='dashboard'),
     url(r'^(?P<project_id>[0-9]*)/origin$', views.origin, name='origin'),
-    url(r'^(?P<project_id>[0-9]*)/updates', views.project_updates, name='updates'),
+    url(r'^(?P<project_id>[0-9]*)/updates', views.project_updates, name='project_updates'),
     url(r'^sitelist$', views.site_list, name='site_list'),
     url(r'^updates', views.corporate_updates, name='updates'),
     url(r'^FRX/radyus$', TemplateView.as_view(template_name='FRX/radyus/index.html')),
