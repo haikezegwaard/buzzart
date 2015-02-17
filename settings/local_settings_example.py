a115769f4da697464a739e7b11f13d289ea99857
haikezegwaard <h.zegwaard@gmail.com>
2015-02-17 17:04:00 +0100

    more plots for synchroon

---
+++ settings/local_settings_example.py
@@ -0,0 +1,7 @@
+
+
+# python-social-auth settings
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '799281546392-hcq2ns4i3sj3dsmlhlqhlqem9h78evkb.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'BF7XhVMue2TXLfF5RbpGXeLK'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['https://www.googleapis.com/auth/analytics.readonly']
+SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {'access_type': 'offline'}
