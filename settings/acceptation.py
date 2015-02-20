a6e4ccf6279b6771f2622e251799d8ed41a09b21
haikezegwaard <h.zegwaard@gmail.com>
2015-02-20 16:32:52 +0100

    altered pwd

--- settings/acceptation.py
+++ settings/acceptation.py
@@ -24,7 +24,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'buzzart',                        # Or path to database file if using sqlite3.
         'USER': 'buzzart',                        # Not used with sqlite3.
-        'PASSWORD': 'sjgn87kfjs',           # Not used with sqlite3.
+        'PASSWORD': 'buzzpass',           # Not used with sqlite3.
         'HOST': '',                           # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                           # Set to empty string for default. Not used with sqlite3.
     },
