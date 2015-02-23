16c7a115acc45819193b0e4d06cf0e8f06c52724
haikezegwaard <h.zegwaard@gmail.com>
2015-02-23 10:37:36 +0100

    heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn buzzart.wsgi --log-file -
---
+++ settings/heroku.py
@@ -0,0 +1,25 @@
+from settings.defaults import *
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Enable Connection Pooling
+#DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
