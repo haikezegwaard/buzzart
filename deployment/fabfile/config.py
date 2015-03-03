3a3cb9fa938fe6da502d56380087d6ec387b5cf2
haike zegwaard <hz@fundament.nl>
2015-03-03 12:14:06 +0100

    changed install user to webdev

--- deployment/fabfile/config.py
+++ deployment/fabfile/config.py
@@ -65,7 +65,7 @@ def acceptation():
     env.project_name = 'buzzart-acc'
     env.hosts = ['django-dev.fundament.nl']
     env.install_dir = '/opt/buzzart/'
-    env.install_user = 'hz'
+    env.install_user = 'webdev'
     env.django_media_root = '/opt/buzzart/media/'
     env.requirements_file = join(env.project_root,
                                  'requirements.txt')
