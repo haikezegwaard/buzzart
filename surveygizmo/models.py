a48d5533538a729c06426ed7584482b284f2c920
haike zegwaard <hz@fundament.nl>
2015-04-07 14:43:41 +0200

    updated models unicode

--- surveygizmo/models.py
+++ surveygizmo/models.py
@@ -11,3 +11,6 @@ class SurveyGizmoAccount(models.Model):
     username = models.CharField(max_length = 300)
     password = models.CharField(max_length = 300)
     
+    def __unicode__(self):
+        return self.project.name    
+    
